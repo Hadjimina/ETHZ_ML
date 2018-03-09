6004b4506f1669ac4da97779ab55c49ee57450cf
Philipp Hadjimina <p.m.hadjimina@gmail.com>
2018-03-09 15:23:30 +0100

    task 1b done

--- Task 1b/task1b.py
+++ Task 1b/task1b.py
@@ -3,6 +3,7 @@ from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import FunctionTransformer
+import sys
 
 
 # 1. get training and test set of X & Y
@@ -14,6 +15,10 @@ X_test = []
 X_train = []
 y_test = []
 y_train= []
+coef = []
+#best lambda is 0.1 for public score, for local score l = 0.074 is better but we get overfitting (i.e. worse public score)
+lamda = [0.1,1,10,100,1000]
+error_matrix = []
 
 transforms = [ lambda x: x, lambda x: x**2,  np.exp,np.cos] #??
 
@@ -30,32 +35,38 @@ for v in X_dummy:
     for f in transforms:
         temp.extend(map(f,v))
     temp.append(1)
-    print(temp[0],temp[5],temp[10],temp[15],temp[20])
+    #print(temp[0],temp[5],temp[10],temp[15],temp[20])
     X.append(temp)
 
+min_error = 6000000
+min_error_index = -1
+index = 0
+kf = KFold(n_splits=10)
+for l in lamda:
+    erro_vec = []
+    for train, test in kf.split(X):
+        #generate training and test sets using kfold gugus
+        X_array = np.array(X)
+        y_array = np.array(y)
+        X_train, X_test, y_train, y_test = X_array[train], X_array[test], y_array[train], y_array[test]
+        # do ridge regressions with lambda = l[i]
+        clf = linear_model.Lasso(alpha=l)
+        clf.fit(X_train, y_train)
+        coef.append(clf.coef_)
+        # Generate prediction using the ridgre regression prediction on our x_test set
+        y_test_predict = clf.predict(X_test)
+        # Calculate error of our prediction
+        RMSE = mean_squared_error(y_test, y_test_predict)**0.5
+        erro_vec.append(RMSE)
+    error = np.mean(np.array(erro_vec))
+    if(error<min_error):
+        min_error = error
+        min_error_index = index
+    index = index+1
+    print("Current error "+str(error)+" Current index "+str(index)+", Min error "+str(min_error)+", Min error index "+str(min_error_index)+" lambda "+str(l) )
 
-# kf = KFold(n_splits=10)
-# erro_vec = []
-# for train, test in kf.split(X):
-#     #generate training and test sets using kfold gugus
-#     X_array = np.array(X)
-#     y_array = np.array(y)
-#     X_train, X_test, y_train, y_test = X_array[train], X_array[test],y_array[train], y_array[test]
-#     # do ridge regressions with lambda = l[i]
-#     regr = linear_model.LinearRegression()
-#     regr.fit(X_train, y_train)
-#     # Generate prediction using the ridgre regression prediction on our x_test set
-#     y_test_predict = regr.predict(X_test)
-#     # Calculate error of our prediction
-#     RMSE = mean_squared_error(y_test, y_test_predict)**0.5
-#     erro_vec.append(RMSE)
-
-#test on entire data
-regr = linear_model.LinearRegression()
-regr.fit(X, y)
-
-
+print("MIN ERROR "+str(min_error)+" min lambda "+str(lamda[min_error_index]))
 f = open('output.csv', 'w')
-for c in regr.coef_:
+for c in coef[index]:
     f.write(str(c)+"\n")
 f.close()
