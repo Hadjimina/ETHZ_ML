6e55a20e395cf27e972c59be6c1d2a21464cf87c
Philipp Hadjimina <p.m.hadjimina@gmail.com>
2018-03-27 19:25:34 +0200

    improvements

--- Task 2/task2.py
+++ Task 2/task2.py
@@ -1,64 +1,66 @@
 import numpy as np
-from sklearn.multiclass import OneVsRestClassifier
+from sklearn import svm
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
-from sklearn.svm import LinearSVC
 
 
 
 # 1. get training and test set of X & Y
 import csv
-X_train = []
-Y_train = []
-X_test  = []
-
-
+X = []
+Y = []
+accVec = []
+avgVec = []
+degreeVec = [2,4,6,8]
 # Training datasets
 with open('train_vale.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     next(readCSV, None)  # skip the header
     for row in readCSV:
-        X_train.append(list( row[2:]))
-        Y_train.append(float(row[1]))
+        X.append(list(map(float, row[2:])))
+        Y.append(float(row[1]))
 
-with open('test_vale.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    next(readCSV, None)  # skip the header
-    for row in readCSV:
-        X_test.append(list(map(float, row[1:])))
+#use of this??
+# with open('test_vale.csv') as csvfile:
+#     readCSV = csv.reader(csvfile, delimiter=',')
+#     next(readCSV, None)  # skip the header
+#     for row in readCSV:
+#         X_test.append(list(map(float, row[1:])))
 
-# Test datasets
-# 10-fold split of train_X
-# RepeatedKFold ??
-
-# kf = KFold(n_splits=10)
-# for l in lamda:
-#     erro_vec = []
-#     for train, test in kf.split(X):
+for d in degreeVec:
+    print("for degree="+str(d))
+    kf = KFold(n_splits=10)
+    for train, test in kf.split(X):
         #generate training and test sets using kfold gugus
-        # X_array = np.array(X)
-        # Y_array = np.array(Y)
-        # X_train, , Y_train, Y_test = X_array[train], X_array[test], Y_array[train], Y_array[test]
-        # # do ridge regressions with lambda = l[i]
-print("potato")
-clf = OneVsRestClassifier(LinearSVC())
-clf.fit(X_train, Y_train)
-        # Generate prediction using the ridgre regression prediction on our x_test set
-Y_test_predict = clf.predict(X_test)
-        # Calculate error of our prediction
-        #acc = accuracy_score(Y_train, Y_test_predict)
-        #print("acc "+str(acc))
-    #     erro_vec.append(RMSE)
-    #
-    # error_matrix.append(erro_vec)
+        X_array = np.array(X)
+        Y_array = np.array(Y)
+        X_train, X_test, Y_train, Y_test = X_array[train], X_array[test], Y_array[train], Y_array[test]
+        # clf = OneVsRestClassifier(LinearSVC()) ---------- results in acc of 0.6
+        clf = svm.SVC(kernel="poly", degree=d) #linear,poly,rbf,sigmoid, precomputed  kernel='poly',degree=3
+        clf.fit(X_train, Y_train)
+        Y_test_predict = clf.predict(X_test)
+        acc = accuracy_score(Y_test, Y_test_predict)
+        accVec.append(acc)
+        # if(acc <= 0.65):
+        #     print("accuracy is SHIT "+str(acc))
+        # else:
+        #     print("accuracy is GOOD "+str(acc))
+    avg = np.mean(np.array(accVec))
+    if (avg < 0.65):
+        print("AVERAGE accuracy is SHIT "+str(avg))
+    else:
+        print("AVERAGE accuracy is GOOD "+str(avg))
+    avgVec.append(avg)
 
-print("potato1")
+
+
+
 f = open('output.csv', 'w')
 q = 2000
 for v in Y_test_predict:
     st = str(q)+", "+str(v)[:-2]+"\n"
-    print(st)
+    #print(st)
     q+=1
     f.write(st)
 f.close()
