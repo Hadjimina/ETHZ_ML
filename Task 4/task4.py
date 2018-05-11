b1af866cf499b1df97060f49dc218b71ca886c82
Jason <jz.friedman@hotmail.com>
2018-05-11 21:57:24 +0200

    added a version using Neural Network

--- Task 4/task4.py
+++ Task 4/task4.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 import math
 import scipy
+from sklearn.model_selection import KFold
 from scipy.sparse.csgraph import connected_components
 from sklearn.semi_supervised import LabelSpreading
 from sklearn.semi_supervised import LabelPropagation
 from sklearn.utils import shuffle
+from sklearn.metrics import mean_squared_error
+
+np.seterr(divide='ignore', invalid='ignore')
 
 #read files
 train_labeled = np.asarray(pd.read_hdf("train_labeled.h5", "train"))
@@ -16,22 +20,33 @@ test = np.asarray(pd.read_hdf("test.h5", "test"))
 X_train_labeled = train_labeled[:,1:]
 Y_train_labeled = train_labeled[:,0]
 
-#fill an array of size len(train_unlabeled) with -1 meaning this are unlabeled
-Y_train_unlabeled = np.empty([len(train_unlabeled)])
-Y_train_unlabeled = np.full_like(Y_train_unlabeled, -1)
+kf = KFold(n_splits=3)
+erro_vec = []
+for train_k, test_k in kf.split(X_train_labeled):
 
-#generate an array of all x Vectors and one with all y values
-X_train = np.concatenate((X_train_labeled, train_unlabeled))
-Y_train = np.concatenate((Y_train_labeled, Y_train_unlabeled))
+    #generate training and test sets using kfold
+    X_train_k, X_test_k, Y_train_k, Y_test_k = X_train_labeled[train_k], X_train_labeled[test_k], Y_train_labeled[train_k], Y_train_labeled[test_k]
 
-#Shuffle X_train and Y_train in same manner to get randomization
-X_train, Y_train = shuffle(X_train, Y_train, random_state=0)
+    #fill an array of size len(train_unlabeled) with -1 meaning this are unlabeled
+    Y_train_unlabeled = np.empty([len(train_unlabeled)])
+    Y_train_unlabeled = np.full_like(Y_train_unlabeled, -1)
 
-#perform semi-supervised learning
-label_prop_model = LabelSpreading( kernel='knn', alpha=0.8, max_iter=4000)
-#label_prop_model = LabelPropagation()
-label_prop_model.fit(X_train, Y_train)
-Y_predict = label_prop_model.predict(test)
+    #generate an array of all x Vectors and one with all y values
+    X_train = np.concatenate((X_train_k, train_unlabeled))
+    Y_train = np.concatenate((Y_train_k, Y_train_unlabeled))
+
+    #perform semi-supervised learning with either LabelSpreading or LabelPropagation
+    #kernels tried: knn, rbf ->knn is better
+    label_prop_model = LabelSpreading(kernel='knn', gamma=20, n_neighbors=7, alpha=0.2, max_iter=4000, tol=0.001, n_jobs=1)
+    #label_prop_model = LabelPropagation(kernel='knn', gamma=20, n_neighbors=7, alpha=None, max_iter=1000000, tol=0.001, n_jobs=1)
+    label_prop_model.fit(X_train, Y_train)
+    Y_predict = label_prop_model.predict(X_test_k)
+
+    #compute prediction error and add to erro_vec
+    RMSE = mean_squared_error(Y_test_k, Y_predict)**0.5
+    erro_vec.append(RMSE)
+
+print(np.mean(erro_vec))
 
 #print result file
 f = open('output.csv', 'w')
---
+++ Task 4/task4nn.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import math
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv1D, LSTM
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+
+# for reproducibility
+np.random.seed(123)
+
+#read files
+train_labeled = np.asarray(pd.read_hdf("train_labeled.h5", "train"))
+train_unlabeled = np.asarray(pd.read_hdf("train_unlabeled.h5", "train"))
+test = np.asarray(pd.read_hdf("test.h5", "test"))
+
+#seperate x vector and y value
+X_train_labeled = train_labeled[:,1:]
+Y_train_labeled = train_labeled[:,0]
+Y_train = tf.keras.utils.to_categorical(Y_train)
+print(test.shape)
+
+#Setup Neural Network sequential model
+model = Sequential()
+
+#Add different layers
+model.add(Dense(512,activation='relu',input_shape=(100,)))
+model.add(Dense(512,activation='relu'))
+model.add(Dropout(0.15))
+model.add(Dense(5,activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+#Fit the labeled training data & predict the unlabeled data & take class with highest prob.
+model.fit(X_train_labeled, Y_train_labeled, batch_size=64, nb_epoch=150, verbose=1)
+Y_predict_unlabeled = model.predict(train_unlabeled)
+Y_predict_unlabeled = np.argmax(np.asarray(Y_predict_unlabeled), 1)
+
+#Concatenate the labeled training set and the now predicted unlabeled training sets
+X_train = np.concatenate(X_train_labeled, train_unlabeled)
+Y_train = np.concatenate(Y_train_labeled, Y_predict_unlabeled)
+
+#Fit the new X and Y vectors in the Neural Network
+model.fit(X_train, Y_train, batch_size=64, nb_epoch=150, verbose=1)
+Y_predict = model.predict(test)
+Y_predict = np.argmax(np.asarray(Y_predict), 1)
+
+#Print result file
+f = open('output.csv', 'w')
+f.write("Id,y\n")
+q = 30000
+for v in Y_predict:
+    st = str(q)+","+str(math.floor(v))+"\n"
+    q+=1
+    f.write(st)
+f.close()
